375e9f414e9db9240ec1854c6035b27ea8458167
Jay Bhalala <bhalalajay722@gmail.com>
2022-07-04 01:10:47 -0500

    skeleton template

---
+++ src/Navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+    Navbar,
+    Nav,
+    Container,
+    Offcanvas
+} from "react-bootstrap";
+
+function BasicNavBar() {
+    return (
+      <div>
+        {["sm"].map((expand) => (
+          <Navbar
+            key={expand}
+            bg="dark"
+            variant="dark"
+            expand={expand}
+            className="mb-3"
+          >
+            <Container fluid>
+            <Navbar.Brand href="/">School Security</Navbar.Brand>
+              <Navbar.Toggle
+                aria-controls={`offcanvasNavbar-expand-${expand}`}
+              />
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-${expand}`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                placement="end"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    Cratos
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/forum">Forum</Nav.Link>
+                    <Nav.Link href="/attendance">Attendance</Nav.Link>
+                    <Nav.Link href="/map">Map</Nav.Link>
+                    <Nav.Link href="/updates">Updates</Nav.Link>
+                  </Nav>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
+        ))}
+      </div>
+  );
+}
+
+export default BasicNavBar;
---
+++ src/components/Map.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Map() {
+    return (
+        <div>
+        <h2>
+            Map
+        </h2>
+      </div>
+    );
+}
+
+export default Map;
