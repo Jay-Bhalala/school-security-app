d49397a42c418cf474c2e8fe8f564a1ef8206638
Gandhiaayush04 <107155503+Gandhiaayush04@users.noreply.github.com>
2022-07-28 17:11:55 -0500

    Forum third commit

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -18,19 +18,43 @@ function Forum() {
           <td>Nikhil Jain</td>
           <td>12:00 am</td>
           <td>Life or Death</td>
-          <button>+</button>
+          <button style={{backgroundColor:"skyBlue", width:"20%"}}>+</button>
         </tr>
         <tr>
           <td>Aayush Gandhi</td>
           <td>4:00 pm</td>
           <td>SHOOTA W A GLOCK</td>
-          <button>+</button>
+          <button style={{backgroundColor:"skyBlue", width:"20%"}}>+</button>
         </tr>
         <tr>
           <td>Ako Saini</td>
           <td>3:00 am</td>
           <td>Supa hot fyea</td>
-          <button>+</button>
+          <button style={{backgroundColor:"skyBlue", width:"20%"}}>+</button>
+        </tr>
+        <tr>
+          <td>Manas Jain</td>
+          <td>12:00 pm</td>
+          <td>Missing Kid</td>
+          <button style={{backgroundColor:"skyblue", width:"20%"}}>+</button>
+        </tr>
+        <tr>
+          <td>Navdha Koladia</td>
+          <td>1:00 pm</td>
+          <td>Missing Kid</td>
+          <button style={{backgroundColor:"skyblue", width:"20%"}}>+</button>
+        </tr>
+        <tr>
+          <td>Cassidy Brown</td>
+          <td>4:00 pm</td>
+          <td>Missing Kid</td>
+          <button style={{backgroundColor:"skyblue", width:"20%"}}>+</button>
+        </tr>
+        <tr>
+          <td>Hardik Singh</td>
+          <td>5:00 pm</td>
+          <td>Missing Kid</td>
+          <button style={{backgroundColor:"skyblue", width:"20%"}}>+</button>
         </tr>
       </tbody>
     </Table>
