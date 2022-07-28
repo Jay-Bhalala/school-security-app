c4e28bb4f4ba76838878e1d4bc40eacfa4b854b1
Gandhiaayush04 <107155503+Gandhiaayush04@users.noreply.github.com>
2022-07-28 16:02:27 -0500

    forum second commit

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -1,13 +1,40 @@
 import React from "react";
 
+import Table from 'react-bootstrap/Table';
+
 function Forum() {
-    return (
-      <div>
-        <h2>
-            Forum
-        </h2>
-      </div>
-    );
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Time Posted</th>
+          <th>Subject</th>
+          <th>View Message</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Nikhil Jain</td>
+          <td>12:00 am</td>
+          <td>Life or Death</td>
+          <button>+</button>
+        </tr>
+        <tr>
+          <td>Aayush Gandhi</td>
+          <td>4:00 pm</td>
+          <td>SHOOTA W A GLOCK</td>
+          <button>+</button>
+        </tr>
+        <tr>
+          <td>Ako Saini</td>
+          <td>3:00 am</td>
+          <td>Supa hot fyea</td>
+          <button>+</button>
+        </tr>
+      </tbody>
+    </Table>
+  );
 }
 
 export default Forum;
