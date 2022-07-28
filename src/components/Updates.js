070bf4d6120a0e932886853d2467fcb93ed457e3
jainnikhil1005 <76232875+jainnikhil1005@users.noreply.github.com>
2022-07-28 15:58:02 -0500

    Update Updates.js

--- src/components/Updates.js
+++ src/components/Updates.js
@@ -1,13 +1,132 @@
-import React from "react";
+import Table from 'react-bootstrap/Table';
 
 function Updates() {
-    return (
-        <div>
-        <h2>
-            Updates
-        </h2>
-      </div>
-    );
+  return (
+    <Table striped>
+      <thead>
+        <tr>
+          <th>Time Posted</th>
+          <th>Name</th>
+          <th>Last Seen</th>
+          <th> Severity Level</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>9:00 am</td>
+          <td>MANAS JAIN</td>
+          <td>cafeteria</td>
+          <td>SEVERE</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>HEMANTH NAGUBHAI</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>ARNAY MAJUMDAR</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>VIKRANT JEEVA</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>ROSHAN VAJINAPALLI</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>KARTHIK NEMANA</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td> DHANUSH JAIN </td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>NIKHIL JAIN</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td> AAYUSH GANDHI </td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>AKSHAT SAINI</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>JAY BHALALA</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>ANIKA DODOAMNE</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>KAPIL TASPA</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>ANJALI GHELANI</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>ANUSHA DUGAD</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+        <tr>
+          <td>7:15</td>
+          <td>MXNXS JXIN</td>
+          <td>Thornton</td>
+          <td>MILD</td>
+        </tr>
+
+      </tbody>
+    </Table>
+  );
 }
 
 export default Updates;
